18498e20866ea6c9a65575ec6038c976c6ad618c
Caitlyn Mcallister <caitlyn.mcallister@noaa.gov>
2023-12-27 18:37:18 -0500

    adds int `i`, `j`, and `ip` to routine getradii

--- src/tracker/gettrk_main.F90
+++ src/tracker/gettrk_main.F90
@@ -11530,6 +11530,7 @@ end program trakmain
     integer               :: iwindix, ipoint, ifcsthr, igrct, igrret
     integer               :: jlatfix, ilonfix, idta, iisa, iwa, iqa, numalloc
     integer               :: numipts, numjpts, jbeg, jend, ibeg, iend, inum, jnum
+    integer               :: i, j, ip
     integer               :: quadct(4), vradius(3,4)
     integer, parameter    :: dp = selected_real_kind(12, 60)
     real(dp), allocatable :: dtemp(:)
