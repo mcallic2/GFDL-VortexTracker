1ea30914074bff581118cc0d328cef228db93369
mcallic2 <caitlyn.mcallister@noaa.gov>
2024-06-27 12:23:40 -0400

    removes unused variable from routine output_phase

--- src/tracker/gettrk_subroutines.f
+++ src/tracker/gettrk_subroutines.f
@@ -12100,7 +12100,7 @@ c
       real    vmaxwind,conv_ms_knots,xminmslp
       integer intlon,intlat,output_fhr
       character  basinid*2,clatns*1,clonew*1
-      integer :: ist, ifcsthour, ioiret
+      integer :: ist, ifcsthour
 
 c     First convert all of the lat/lon values from reals into integers.
 c     These integer values must be 10x their real value (eg. 125.4 will
