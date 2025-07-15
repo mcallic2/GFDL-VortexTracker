aeffe8d092e701e5df6efb02040eb6d4192d363c
Caitlyn Mcallister <caitlyn.mcallister@noaa.gov>
2025-07-15 12:00:12 -0400

    create cleanworkdir.sh; add code to move trak.* files to output_files dir

---
+++ run/cleanworkdir.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# -------------------------------------------------------------------------------------------------
+# CLEAN UP WORK DIRECTORY
+
+# create directory to put atcf output files
+export outputdir=${wdir}/output_files
+if [ ! -d ${outputdir} ]; then mkdir -p ${outputdir}; fi
+
+# move all output files from wdir to output_files dir
+mv trak.* ${outputdir}
