c2e1d0177d6348f35e36afef3b234caf4097d9cc
Caitlyn Mcallister <caitlynmcallister@Mac.lan>
2025-11-10 11:27:17 -0500

    create subscripts directory & add necessary scripts

---
+++ run/netcdf/subscripts/IOfiles_ncdf.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+set -x
+# -------------------------------------------------------------------------------------------------
+# SET UP INPUT/OUTPUT FILES
+
+# go into work dir to add files
+cd ${wdir}
+
+# link various files that are either needed as input to the tracker or are output from the tracker
+cp ${namelist} namelist.gettrk
+ln -s -f namelist.gettrk                                       fort.555
+
+ln -s -f ${ncdf_filename}                                      fort.11
+if [ ${read_separate_land_mask_file} = 'y' ]; then
+  ln -s -f ${ncdf_ls_mask_filename}                            fort.17
+fi
+
+if [ -s ${wdir}/vitals.${initymdh} ]; then
+  cp ${wdir}/vitals.${initymdh} ${wdir}/tcvit_rsmc_storms.txt
+else
+  > ${wdir}/tcvit_rsmc_storms.txt
+fi
+
+if [ -s ${wdir}/genvitals.upd.${atcfname}.${pdy}${hh} ]; then
+  cp ${wdir}/genvitals.upd.${atcfname}.${pdy}${hh} ${wdir}/tcvit_genesis_storms.txt
+else
+  > ${wdir}/tcvit_genesis_storms.txt
+fi
+
+ln -s -f ${rundir}/leadtimes.txt                               fort.15
+
+if [ ${vortex_tilt_flag} = 'y' ]; then
+  ln -s -f ${rundir}/vortex_tilt_levs.txt                      fort.18
+  ln -s -f ${rundir}/vortex_tilt_vars.txt                      fort.33
+  ln -s -f ${wdir}/trak.${atcfname}.vortex_tilt.${pdy}${hh}    fort.82
+fi
+
+if [ ${trkrtype} = 'tracker' ]; then
+  ln -s -f ${wdir}/trak.${atcfname}.all.${pdy}${hh}            fort.61
+  ln -s -f ${wdir}/trak.${atcfname}.atcf.${pdy}${hh}           fort.62
+  ln -s -f ${wdir}/trak.${atcfname}.atcfunix.${pdy}${hh}       fort.64
+  ln -s -f ${wdir}/trak.${atcfname}.atcfunix_ext.${pdy}${hh}   fort.68
+  ln -s -f ${wdir}/trak.${atcfname}.atcf_hfip.${pdy}${hh}      fort.69
+  ln -s -f ${wdir}/trak.${atcfname}.parmfix.${pdy}${hh}        fort.81
+else  # trkrtype = tcgen
+  ln -s -f ${wdir}/trak.${atcfname}.all.${pdy}${hh}            fort.61
+  ln -s -f ${wdir}/trak.${atcfname}.atcf.${pdy}${hh}           fort.62
+  ln -s -f ${wdir}/trak.${atcfname}.atcfunix.${pdy}${hh}       fort.64
+  ln -s -f ${wdir}/trak.${atcfname}.atcf_gen.${pdy}${hh}       fort.66
+  ln -s -f ${wdir}/trak.${atcfname}.atcfunix_ext.${pdy}${hh}   fort.68
+  ln -s -f ${wdir}/trak.${atcfname}.atcf_hfip.${pdy}${hh}      fort.69
+  ln -s -f ${wdir}/trak.${atcfname}.parmfix.${pdy}${hh}        fort.81
+fi
+
+if [ ${atcfname} = 'aear' ]
+then
+  ln -s -f ${wdir}/trak.${atcfname}.initvitl.${pdy}${hh}       fort.65
+fi
+
+if [ ${write_vit} = 'y' ]
+then
+  ln -s -f ${wdir}/output_genvitals.${atcfname}.${pdy}${hh}    fort.67
+fi
+
+if [ ${phaseflag} = 'y' ]; then
+  ln -s -f ${wdir}/trak.${atcfname}.cps_parms.${pdy}${hh}      fort.71
+fi
+
+if [ ${structflag} = 'y' ]; then
+  ln -s -f ${wdir}/trak.${atcfname}.structure.${pdy}${hh}      fort.72
+  ln -s -f ${wdir}/trak.${atcfname}.fractwind.${pdy}${hh}      fort.73
+  ln -s -f ${wdir}/trak.${atcfname}.pdfwind.${pdy}${hh}        fort.76
+fi
+
+if [ ${ikeflag} = 'y' ]; then
+  ln -s -f ${wdir}/trak.${atcfname}.ike.${pdy}${hh}            fort.74
+fi
+
+if [ ${trkrtype} = 'midlat' -o ${trkrtype} = 'tcgen' -o ${trkrtype} = 'tracker' ]; then
+  ln -s -f ${wdir}/trkrmask.${atcfname}.${pdy}${hh}            fort.77
+fi
+
+# -------------------------------------------------------------------------------------------------
+set +x
---
+++ run/netcdf/subscripts/compile_ncdf.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# -------------------------------------------------------------------------------------------------
+# BUILD & COMPILE TRACKER EXECUTABLES
+
+# create build directory & move into it
+export builddir=${codedir}/build
+if [ ! -d ${builddir} ]; then mkdir -p ${builddir}; fi
+cd ${builddir}
+
+# remove contents of build dir for fresh compilation
+if [ -d ${builddir} ]; then rm -rf {*,*}; fi
+
+# determine which system user is on
+if [ -z ${which_system} ]; then             # blank
+  echo "not using known rdhpc system; not using docker image"
+elif [ ${which_system} = "docker" ]; then    # using container
+  echo "what goes here?"
+else                                        # using rdhpc system
+  export modsetup=${codedir}/modulefile-setup
+  cd ${modsetup}
+  source ${which_system}-setup.sh
+  module list
+fi
+
+# build & compile source code
+if [ ${which_system} = "gaea" ]; then
+  cmake .. -DCMAKE_Fortran_COMPILER=ftn -DCMAKE_C_COMPILER=cc
+elif [ ${which_system} = "analysis" ] \
+  || [ ${which_system} = "hera" ]     \
+  || [ ${which_system} = "hercules" ] \
+  || [ ${which_system} = "jet" ]      \
+  || [ ${which_system} = "orion" ]    \
+  || [ ${which_system} = "ursa" ]     \
+  || [ ${which_system} = "wcoss2" ]; then
+  cmake .. -DCMAKE_Fortran_COMPILER=ifx -DCMAKE_C_COMPILER=icx
+elif [ ${which_system} = "docker" ]; then
+  cmake ..
+else    # not on known system
+    echo "this might have to be edited"
+    cmake ..
+fi
+
+# compile source code
+make
+
+# add success/fail compilation message
+# CAITLYN - don't know if this will work or if it's needed
+if [ $? -eq 0 ]; then
+  echo "COMPILATION SUCCESSFUL"
+else
+  echo "COMPILATION ERROR"
+fi
+
+# install executables in exec/ directory
+make install
+
+# move back into run directory
+cd ${rundir}
