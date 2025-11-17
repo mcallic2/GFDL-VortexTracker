97bc862bfdd653b8008bebf7f9c414e93678d208
Caitlyn Mcallister <caitlynmcallister@Mac.lan>
2025-11-17 16:39:31 -0500

    edit comments

--- run/docker/use-docker.sh
+++ run/docker/use-docker.sh
@@ -3,7 +3,7 @@
 set -x
 # -------------------------------------------------------------------------------------------------
 
-# Check if the directory exists and is not empty
+# Check if user's run directory is set to netcdf or grib
 if [[ -n ${rundir} && -d ${rundir} ]]; then
   echo "Your run directory is: ${rundir}"
   export usencdf=${homedir}/run/netcdf
@@ -12,6 +12,7 @@ else
   echo "The run directory does not exist or is empty"
 fi
 
+# Check expected input data file type
 if [ ${rundir} == ${usencdf} ]; then
   echo "RUNNING WITH NETCDF DATA"
 else
