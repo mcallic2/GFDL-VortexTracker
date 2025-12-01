d862dbe335182815f19fb23122c175e4189e8710
mcallic2 <caitlyn.mcallister@noaa.gov>
2025-12-01 11:59:16 -0500

    grib2 multi gen

--- run/grib/run-grib.sh
+++ run/grib/run-grib.sh
@@ -1,9 +1,9 @@
 #!/bin/bash --login
-#SBATCH  -o output/multrk.out
-#SBATCH  -J g2trk
+#SBATCH  -o output/mulgen.out
+#SBATCH  -J g2gen
 #SBATCH  --time=30       # time limit in minutes
 #SBATCH  --ntasks=1
-#SBATCH  --partition=xjet
+#SBATCH  --partition=kjet
 #SBATCH  -A hfip-gfdl
 
 # print line numbers in std out
@@ -27,7 +27,7 @@ export rundir=${homedir}/run/grib
 export codedir=${homedir}/code
 export arcvitals=${homedir}/archived_vitals
 export execdir=${codedir}/exec
-export workdir=${rundir}/work/grib2/multtrk
+export workdir=${rundir}/work/grib2/multgen
 
 # Name of rdhpc system (gaea, analysis, wcoss2, etc.), docker, or blank if on personal system
 export which_system='jet'
@@ -45,7 +45,7 @@ export initymdh=2023092912
 export fcsthrs='0 6 12 18 24 30 36 42 48 54 60 66 72 78 84 90 96 102 108 114 120 126'
 
 # trkrtype=tracker for known storms, trkrtype=tcgen for genesis runs
-export trkrtype='tracker'
+export trkrtype='tcgen'
 
 # gribver=1 for GRIB1 data; gribver=2 for GRIB2 data
 export gribver=2
