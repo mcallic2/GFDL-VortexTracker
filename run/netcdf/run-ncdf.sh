e17a2febf29750ecbaf1d5e5fc12e5e4861bab6e
mcallic2 <caitlyn.mcallister@noaa.gov>
2025-12-01 14:18:22 -0500

    tshld trk

--- run/netcdf/run-ncdf.sh
+++ run/netcdf/run-ncdf.sh
@@ -19,7 +19,7 @@ export homedir=/mnt/lfs5/HFIP/hfip-gfdl/Caitlyn.Mcallister/genericscripts/dec1
 
 # After adding the name of your data file & the path to it, please make sure to
 # navigate into subscripts/name_ncdfvars.sh and follow the directions
-export ncdf_filename='shieldcombined.nc'
+export ncdf_filename='tshldcombined.nc'
 export datadir=/mnt/lfs5/HFIP/hfip-gfdl/Caitlyn.Mcallister/ncdfdata/combined
 
 # If you already have a tcvitals file, add path and name below.
@@ -31,7 +31,7 @@ export rundir=${homedir}/run/netcdf
 export codedir=${homedir}/code
 export arcvitals=${homedir}/archived_vitals
 export execdir=${codedir}/exec
-export workdir=${rundir}/work/shld
+export workdir=${rundir}/work/tshd
 
 # Name of rdhpc system (gaea, analysis, wcoss2, etc.), docker, or blank if on personal system
 export which_system='jet'
@@ -42,23 +42,23 @@ if [ ${which_system} = 'docker' ]; then
 fi
 
 # Initialization forecast date/time matching your data
-export initymdh=2024092500
+export initymdh=2023082900
 
 # trkrtype=tracker for known storms, trkrtype=tcgen for genesis runs
 export trkrtype='tracker'
 
 # ATCF name of model (4 char long)
-export atcfname='shld'
+export atcfname='tshd'
 
 # Variables to declare the model's grid and nesting configurations
-export modtyp='global'         # 'global' or 'regional'
+export modtyp='regional'         # 'global' or 'regional'
 export nest_type='fixed'      # 'moveable' or 'fixed'
 
 # Set 'use_land_mask' = 'y' AND 'read_serperate_land_mask_file' = 'y' if landmask file is needed.
-export use_land_mask='y'
-export read_separate_land_mask_file='y'
+export use_land_mask='n'
+export read_separate_land_mask_file='n'
 # Add path to landmask file below if 'y' to above variables, leave blank if 'n'
-export ncdf_ls_mask_filename=${datadir}/SLMSKsfc_SHiELD_C768.RT2020a_GFSv16.nc
+export ncdf_ls_mask_filename=''
 
 # 'vortex_tilt_flag' = 'y'/'n'
 # if developer wants to run vortex tilt diagnostics, set to 'y'. If set to 'n', the following if-statement will set
