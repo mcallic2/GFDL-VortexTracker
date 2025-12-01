1fe07fd597e51b74c29d08f31ce9246ed82aa145
mcallic2 <caitlyn.mcallister@noaa.gov>
2025-12-01 13:56:39 -0500

    tshd tracker

--- run/netcdf/run-ncdf.sh
+++ run/netcdf/run-ncdf.sh
@@ -1,5 +1,10 @@
 #!/bin/bash --login
-# add batch commands if needed
+#SBATCH  -o output/tshdtrk.out
+#SBATCH  -J tshd
+#SBATCH  --time=30       # time limit in minutes
+#SBATCH  --ntasks=1
+#SBATCH  --partition=kjet
+#SBATCH  -A hfip-gfdl
 
 # print line numbers in std out
 export PS4=' line $LINENO: '
@@ -10,12 +15,12 @@ set -x
 # PLEASE TAKE TIME TO READ COMMENTS AND EDIT ACCORDINGLY
 
 # Set directory paths
-export homedir=
+export homedir=/mnt/lfs5/HFIP/hfip-gfdl/Caitlyn.Mcallister/genericscripts/dec1
 
 # After adding the name of your data file & the path to it, please make sure to
 # navigate into subscripts/name_ncdfvars.sh and follow the directions
-export ncdf_filename=''
-export datadir=
+export ncdf_filename='tshldcombined.nc'
+export datadir=/mnt/lfs5/HFIP/hfip-gfdl/Caitlyn.Mcallister/ncdfdata/combined
 
 # If you already have a tcvitals file, add path and name below.
 # If the file has not been created yet, leave this blank and the tcvitals_ncdf.sh script will create one
@@ -26,10 +31,10 @@ export rundir=${homedir}/run/netcdf
 export codedir=${homedir}/code
 export arcvitals=${homedir}/archived_vitals
 export execdir=${codedir}/exec
-export workdir=${rundir}/work
+export workdir=${rundir}/work/tshd
 
 # Name of rdhpc system (gaea, analysis, wcoss2, etc.), docker, or blank if on personal system
-export which_system=''
+export which_system='jet'
 
 if [ ${which_system} = 'docker' ]; then
   export dockerdir=${homedir}/run/docker
@@ -37,28 +42,28 @@ if [ ${which_system} = 'docker' ]; then
 fi
 
 # Initialization forecast date/time matching your data
-export initymdh=
+export initymdh=2023082900
 
 # trkrtype=tracker for known storms, trkrtype=tcgen for genesis runs
-export trkrtype=''
+export trkrtype='tracker'
 
 # ATCF name of model (4 char long)
-export atcfname=''
+export atcfname='tshd'
 
 # Variables to declare the model's grid and nesting configurations
-export modtyp=''         # 'global' or 'regional'
-export nest_type=''      # 'moveable' or 'fixed'
+export modtyp='regional'         # 'global' or 'regional'
+export nest_type='fixed'      # 'moveable' or 'fixed'
 
 # Set 'use_land_mask' = 'y' AND 'read_serperate_land_mask_file' = 'y' if landmask file is needed.
-export use_land_mask=''
-export read_separate_land_mask_file=''
+export use_land_mask='n'
+export read_separate_land_mask_file='n'
 # Add path to landmask file below if 'y' to above variables, leave blank if 'n'
 export ncdf_ls_mask_filename=''
 
 # 'vortex_tilt_flag' = 'y'/'n'
 # if developer wants to run vortex tilt diagnostics, set to 'y'. If set to 'n', the following if-statement will set
 # the tilt_parm and threshold variables values automatically
-export vortex_tilt_flag=''
+export vortex_tilt_flag='n'
 export vortex_tilt_parm=''
 export vortex_tilt_allow_thresh=
 
