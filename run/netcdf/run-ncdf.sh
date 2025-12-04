8d8d9cd8cb2897973ae4d152d6ad92c244c95d88
mcallic2 <caitlyn.mcallister@noaa.gov>
2025-12-04 12:40:57 -0500

    shield gen

--- run/netcdf/run-ncdf.sh
+++ run/netcdf/run-ncdf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash --login
-#SBATCH -o output/tshdtrk.out
-#SBATCH -J tshtrk
+#SBATCH -o output/shldgen.out
+#SBATCH -J shlgen
 #SBATCH --export=ALL
 #SBATCH --time=30   # time limit in minutes
 #SBATCH --nodes=1
@@ -18,12 +18,12 @@ set -x
 # PLEASE TAKE TIME TO READ COMMENTS AND EDIT ACCORDINGLY
 
 # Set directory paths
-export homedir=/gpfs/f5/gfdl_f/scratch/Caitlyn.Mcallister/genericscripts/dec3
+export homedir=/gpfs/f5/gfdl_f/scratch/Caitlyn.Mcallister/genericscripts/dec4
 
 # After adding the name of your data file & the path to it, please make sure to
 # navigate into subscripts/name_ncdfvars.sh and follow the directions
-export ncdf_filename='tshldcombined.nc'
-export datadir=/gpfs/f5/gfdl_f/scratch/Caitlyn.Mcallister/ncdf_inputdata/tshd_combined
+export ncdf_filename='shieldcombined.nc'
+export datadir=/gpfs/f5/gfdl_f/scratch/Caitlyn.Mcallister/ncdf_inputdata/shld_combined
 
 # If you already have a tcvitals file, add path and name below.
 # If the file has not been created yet, leave this blank and the tcvitals_ncdf.sh script will create one
@@ -45,23 +45,23 @@ if [ ${which_system} = 'docker' ]; then
 fi
 
 # Initialization forecast date/time matching your data
-export initymdh=2023082900
+export initymdh=2024092500
 
 # trkrtype=tracker for known storms, trkrtype=tcgen for genesis runs
-export trkrtype='tracker'
+export trkrtype='tcgen'
 
 # ATCF name of model (4 char long)
-export atcfname='tshd'
+export atcfname='shld'
 
 # Variables to declare the model's grid and nesting configurations
-export modtyp='regional'         # 'global' or 'regional'
+export modtyp='global'         # 'global' or 'regional'
 export nest_type='fixed'      # 'moveable' or 'fixed'
 
 # Set 'use_land_mask' = 'y' AND 'read_serperate_land_mask_file' = 'y' if landmask file is needed.
-export use_land_mask='n'
-export read_separate_land_mask_file='n'
+export use_land_mask='y'
+export read_separate_land_mask_file='y'
 # Add path to landmask file below if 'y' to above variables, leave blank if 'n'
-export ncdf_ls_mask_filename=''
+export ncdf_ls_mask_filename=${datadir}/SLMSKsfc_SHiELD_C768.RT2020a_GFSv16.nc
 
 # 'vortex_tilt_flag' = 'y'/'n'
 # if developer wants to run vortex tilt diagnostics, set to 'y'. If set to 'n', the following if-statement will set
