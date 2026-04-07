02410d61793cec8c48e7ea1b8ca5ed6648544b0e
mcallic2 <caitlyn.mcallister@noaa.gov>
2026-04-07 14:55:08 -0400

    add gcc env for hera

--- code/system-envs/gcc/hera.sh
+++ code/system-envs/gcc/hera.sh
@@ -2,5 +2,35 @@
 
 # This script is designed to load all necessary modules/libraries
 # to compile and run the tracker on HERA with the gcc compiler.
-# The libraries are loaded using the spack environment v2026.01
+# The libraries are loaded using the spack environment v1.9.2
 # -------------------------------------------------------------------
+
+module use /contrib/spack-stack/spack-stack-1.9.2/envs/ue-gcc-13.2.0/install/modulefiles/Core/
+module load stack-gcc/13.2.0
+module load gnu/13.2.0
+
+module load cmake/3.28.1
+
+module load hdf5/1.14.3
+module load netcdf/4.9.2
+module load jasper/2.0.32
+module load zlib/1.2.11
+module load libpng/1.6.37
+module load g2c/2.1.0
+module load g2/3.5.1
+module load g2tmpl/1.13.0
+module load w3emc/2.10.0
+
+# netcdf specific libs
+module load nco/5.1.6
+module load cdo/2.3.0
+export ncdump=/apps/netcdf/4.9.2/gnu_13.2.0_seq/bin/ncdump
+
+# grib specific libs
+module load grib-util/1.4.0
+module load wgrib/1.8.1.0b
+module load wgrib2/3.6.0
+
+# set flags for compilations
+export FC=gfortran
+export CC=gcc
