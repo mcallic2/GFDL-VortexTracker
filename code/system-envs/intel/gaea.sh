5a669b8214e7792d36df6421480c04e37451fa2b
mcallic2 <caitlyn.mcallister@noaa.gov>
2026-04-07 13:25:05 -0400

    add updated gaea module env; no gcc

--- code/system-envs/intel/gaea.sh
+++ code/system-envs/intel/gaea.sh
@@ -4,3 +4,30 @@
 # to compile and run the tracker on GAEA with the intel-classic compiler.
 # The libraries are loaded using the spack environment v1.8.0
 # -------------------------------------------------------------------
+
+module use /ncrc/proj/epic/spack-stack/spack-stack-1.8.0/envs/ue-intel-2021.10.0/install/modulefiles/Core/
+module load stack-intel/2023.2.0
+
+module load cray-hdf5/1.14.3.5
+module load cray-netcdf/4.9.0.13
+module unload cray-libsci/25.03.0
+
+module load libpng/1.6.37
+module load jasper/2.0.32
+module load zlib-ng/2.1.6
+module load g2c/1.6.4
+module load g2/3.5.1
+module load g2tmpl/1.13.0
+module load bacio/2.4.1
+module load w3emc/2.10.0
+
+# netcdf specific libs
+module load nco/5.1.9
+module load cdo/2.3.0-omp
+
+export ncdump=/opt/cray/pe/netcdf/4.9.0.13/bin/ncdump
+
+# grib specific libs
+module load grib-util/1.4.0
+module load wgrib/1.8.5
+module load wgrib2/3.1.1
