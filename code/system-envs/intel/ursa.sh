4cc6f9a1367bf6a2caf60da99e8427e6ecb8f094
mcallic2 <caitlyn.mcallister@noaa.gov>
2026-04-08 10:58:24 -0400

    add intel module env for ursa

--- code/system-envs/intel/ursa.sh
+++ code/system-envs/intel/ursa.sh
@@ -1,6 +1,38 @@
 #!/bin/bash
 
 # This script is designed to load all necessary modules/libraries
-# to compile and run the tracker on URSA with the intel-classic compiler.
-# The libraries are loaded using the spack environment v1.8.0
+# to compile and run the tracker on URSA with the intel-oneapi compiler.
+# The libraries are loaded using the spack environment v1.9.3
 # -------------------------------------------------------------------
+
+module use /contrib/spack-stack/spack-stack-1.9.3/envs/ue-oneapi-2024.2.1/install/modulefiles/Core/
+module load stack-oneapi/2024.2.1
+module load intel-oneapi-compilers/2025.3.1
+
+module load cmake/3.30.2
+
+module load hdf5/1.14.3
+module load netcdf-c/4.9.2
+module load netcdf-fortran/4.6.1
+
+module load zlib/1.2.13
+module load libpng/1.6.37
+module load g2c/2.1.0
+module load g2/3.5.1
+module load g2tmpl/1.13.0
+module load jasper/2.0.32
+module load bacio/2.4.1
+module load w3emc/2.10.0
+
+# netcdf specific libs
+module load nco/5.2.4
+module load cdo/2.4.2
+export ncdump=/apps/spack-2024-12/linux-rocky9-x86_64/oneapi-2025.3.1/netcdf-c-4.9.2-bnv72ii4gwaskzhvs5njhwbbgyl4f6v2/bin/ncdump
+
+# grib specific data
+module load grib-util/1.5.0
+module load wgrib2/3.1.3_wmo
+
+# set flags for compilations
+export FC=ifx
+export CC=icx
